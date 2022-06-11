d5a81c2ded200fdaa1747ecf9f5c10c35e36ebe3
Chris Richardson <chris@chrisrichardson.net>
2022-06-10 19:24:07 -0700

    Upgraded dependencies, use orb

--- mysql-cli.sh
+++ mysql-cli.sh
@@ -2,5 +2,5 @@
 
 docker run $*  \
    --network eventuateexamplesjavacustomersandorders_default \
-   --name mysqlterm  --rm mysql:5.7.13 \
+   --name mysqlterm  --rm mysql/mysql-server:8.0.27-1.2.6-server \
    sh -c 'exec mysql -hmysql -P3306 -uroot -prootpassword eventuate'
